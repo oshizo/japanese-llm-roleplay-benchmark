9c9066d2315de635493e954eeb8a342f5a96ee33
oshizo <114740986+oshizo@users.noreply.github.com>
2023-09-17 22:43:03 +0900

    updates

--- utils.py
+++ utils.py
@@ -8,3 +8,13 @@ def download_erp_file():
         urllib.request.urlretrieve(
             "https://furoriented.org/erp/situations_erp.jsonl", "situations_erp.jsonl"
         )
+
+
+def download_erp_response(filename):
+    if not os.path.exists(f"./responses_erp/{filename}"):
+        import urllib.request
+
+        urllib.request.urlretrieve(
+            f"https://furoriented.org/erp/responses_erp/{filename}",
+            f"./responses_erp/{filename}",
+        )
